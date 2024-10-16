5dbe17f001f9b9d834001b007386ee01fdad6620
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-10-16 23:01:51 +0200

    update

--- Music Player-Chumisa Haya/src/context/PlayerContext.jsx
+++ Music Player-Chumisa Haya/src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from '../assets/assets'; 
 
 
@@ -19,19 +19,39 @@ const PlayerContextProvider = (props) => {
         },
         totalTime: {
             second: 0,
-            minute: 0,
+            minute: 0
         }
-    })
+    });
 
     const play = () => {
         audioRef.current.play();
         setPlayStatus(true)
-    }
+    };
 
     const pause = () => {
         audioRef.current.play();
         setPlayStatus(false);
-    } 
+    };
+
+    useEffect(()=>{
+        setTime(()=> {
+
+            audioRef.current.ontimeupdate = () => {
+                setTime({
+                    currentTime: {
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60)
+                },
+                totalTime: {
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60)
+                }
+            })
+         
+         }
+
+        }, 1000);
+    },[audioRef])
 
     const contextValue = {
         audioRef,
