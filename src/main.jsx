b8dac54fd339e16c7d95773bac8deaeeff2a8961
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-10-23 21:40:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import PlayerContextProvider from './context/PlayerContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL || "/"}>
     <PlayerContextProvider>
     <App />
     </PlayerContextProvider>
